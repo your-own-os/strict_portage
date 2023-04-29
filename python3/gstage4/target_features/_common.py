6f0e0abe44c624cc8d5bd3e3c94b55ce06d66a30
fpemud <fpemud@sina.com>
2023-04-29 01:55:30 +0000

    fix
    Signed-off-by: fpemud <fpemud@sina.com>

--- python3/gstage4/target_features/_common.py
+++ python3/gstage4/target_features/_common.py
@@ -638,6 +638,9 @@ class PreferWayland:
 
         target_settings.pkg_mask_files["10-prefer-wayland"] = self._maskFileContent.strip("\n") + "\n"
 
+        if not self._xwayland:
+            target_settings.repo_postsync_patch_directories.append(os.path.join(host_info.repo_postsync_patch_source_dir, "remove-x11"))
+
     _useFileContent = """
 # we use wayland
 */*                                           wayland
