060599fc1b580ce81505d1c59421572c34def846
fpemud <fpemud@sina.com>
2023-06-16 20:30:20 +0800

    fix

---
+++ libexec/patch-repository.d/vscodium-use-wayland/gentoo/app-editors/vscodium/modify.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+import glob
+import pathlib
+
+try:
+    # what to insert (with blank line in the beginning and the end)
+    buf2 = r"""
+sed -i 's/kmscon -l/kmscon --listen -l/g' docs/kmscon.service.in
+"""
+    buf2 = buf2.replace("\n", "\n\t")
+    buf2 += "\n"
+
+    for fn in glob.glob("*.ebuild"):
+        buf = pathlib.Path(fn).read_text()
+
+        # insert to the end of src_install()
+        pos = buf.find("src_prepare() {")
+        if pos == -1:
+            raise ValueError()
+        pos = buf.find("\n}\n", pos)
+        if pos == -1:
+            raise ValueError()
+        pos += 1
+
+        # do insert
+        buf = buf[:pos] + buf2 + buf[pos:]
+        with open(fn, "w") as f:
+            f.write(buf)
+except ValueError:
+    print("outdated")
