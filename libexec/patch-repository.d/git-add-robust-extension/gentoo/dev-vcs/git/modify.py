33ae9787e8957f10483b721feacf153af0c5720d
fpemud <fpemud@sina.com>
2023-06-24 16:24:08 +0800

    fix

--- libexec/patch-repository.d/git-add-robust-extension/gentoo/dev-vcs/git/modify.py
+++ libexec/patch-repository.d/git-add-robust-extension/gentoo/dev-vcs/git/modify.py
@@ -13,7 +13,7 @@ shutil.copyfile(os.path.join(selfDir, "files", "git"), os.path.join("files", "gi
 try:
     # what to insert (with blank line in the beginning and the end)
     buf2 = r"""
-dobin "${FILESDIR}/git"     \# /usr/bin/git is the same as /usr/libexec/git-core/git, replace /usr/bin/git with our own robust script that calls /usr/libexec/git-core/git
+dobin "${FILESDIR}/git"     # /usr/bin/git is the same as /usr/libexec/git-core/git, replace /usr/bin/git with our own robust script that calls /usr/libexec/git-core/git
 """
     buf2 = buf2.replace("\n", "\n\t")
     buf2 += "\n"
