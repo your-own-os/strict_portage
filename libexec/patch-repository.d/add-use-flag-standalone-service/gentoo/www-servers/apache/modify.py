8dbd53638fbefabf55e65746cdeec8ed180f7525
fpemud <fpemud@sina.com>
2023-09-25 01:08:26 +0000

    fix
    Signed-off-by: fpemud <fpemud@sina.com>

--- libexec/patch-repository.d/add-use-flag-standalone-service/gentoo/www-servers/apache/modify.py
+++ libexec/patch-repository.d/add-use-flag-standalone-service/gentoo/www-servers/apache/modify.py
@@ -29,7 +29,7 @@ fi
         buf = buf[:pos] + buf2 + buf[pos:]
 
         # insert new use flag
-        buf += '\nIUSE="${IUSE} standalone-service"\n'
+        buf += '\nIUSE="${IUSE} +standalone-service"\n'
 
         with open(fn, "w") as f:
             f.write(buf)
