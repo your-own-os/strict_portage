f5d78100faf984e8a71bf34446b709537151c513
fpemud <fpemud@sina.com>
2024-04-27 07:04:31 +0800

    fix

--- libexec/patch-repository.d/systemd-disable-kmod-static-nodes/gentoo/sys-apps/systemd/modify.py
+++ libexec/patch-repository.d/systemd-disable-kmod-static-nodes/gentoo/sys-apps/systemd/modify.py
@@ -10,7 +10,7 @@ try:
 # we don't use kmod-static-nodes
 find "${D}" -name "*static-nodes*" | xargs rm -rf
 find "${D}" -name "*systemd-tmpfiles-setup-dev*" | xargs rm -rf
-sed -i -E ':x; /\\$/ { N; s/\\\n +//; tx }'   ${D}/usr/lib/udev/rules.d/{50-udev-default,70-uaccess}.rules                                          # combind lines ends with \ to one line
+sed -i -E ':x; /\\$/ { N; s/\\\n +//; tx }'   ${D}/usr/lib/udev/rules.d/{50-udev-default,70-uaccess}.rules                                          # combind lines ending with \ to one line
 sed -i -E 's/, OPTIONS\+="static_node=.+"//g' ${D}/usr/lib/udev/rules.d/*.rules                                                                     # remove rule parts containing "static-nodes"
 sed -E '/^#.*static[_-]node/d' ${D}/usr/lib/udev/rules.d/50-udev-default.rules | cat -s > ${D}/usr/lib/udev/rules.d/50-udev-default.rules.2         # remove comments containing "static-nodes"
 mv ${D}/usr/lib/udev/rules.d/50-udev-default.rules.2 ${D}/usr/lib/udev/rules.d/50-udev-default.rules
