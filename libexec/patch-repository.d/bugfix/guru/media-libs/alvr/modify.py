d69e355febed8efc5ad4b42a70ce215df931d59d
fpemud <fpemud@sina.com>
2024-08-05 22:37:44 +0800

    fix

---
+++ libexec/patch-repository.d/bugfix/guru/media-libs/alvr/modify.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+import os
+import glob
+import subprocess
+
+try:
+    selfDir = os.path.dirname(os.path.realpath(__file__))
+    for fullfn in glob.glob(os.path.join(selfDir, "*.ebuild")):
+        if os.path.exists(os.path.basename(fullfn)):
+            raise ValueError()
+    subprocess.check_call("cp %s ." % (fullfn))
+except ValueError:
+    print("outdated")
