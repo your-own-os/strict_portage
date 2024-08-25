05bc43b4dbbb2cafebc70b6aede6c072c85a7b28
fpemud <fpemud@sina.com>
2024-08-25 11:03:44 +0800

    fix

--- python3/gstage4/target_features/_common.py
+++ python3/gstage4/target_features/_common.py
@@ -780,7 +780,6 @@ class PreferWayland:
     _xwaylandContent = """
 # of course, we also use X when we have to
 app-emulation/wine-vanilla                    X                               # wine has no wayland support, it has to use Xwayland
-app-emulation/wine-staging                    X
 dev-util/electron                             X -wayland                      # electron wayland support needs ozone which is broken now
 gui-wm/wayfire                                X                               # enable Xwayland
 app-misc/ddcutil                              X                               # drm needs X?
